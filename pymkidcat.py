27156e381232cef67d15d7cae395422534c279b7
whiteFly <32365615+Sinf0r0s0@users.noreply.github.com>
2018-11-16 13:18:58 -0200

    Update pymkidcat.py
    added number of spawning to_bute() function, based on your processor cores end remove empty lines

--- pymkidcat.py
+++ pymkidcat.py
@@ -3,38 +3,46 @@ import sys
 from hashlib import pbkdf2_hmac, sha1
 import argparse
 import datetime
-from multiprocessing import Process
+import multiprocessing
+import os
 
-print ("running...\n")
+start = datetime.datetime.now()   
 
-start = datetime.datetime.now()
+def to_brute(sem):
+    sem.acquire()
 
-def to_brute(pmkid, essid, msg):
     with open(wordlist) as w:
         for line in w:
             line = line.strip()
             if (len(line) <= 7):
                 continue
-            
-            # pbkdf2_gen
-            pmk = (pbkdf2_hmac(hash_name='sha1', password=line,salt=essid,iterations=4096,dklen=32))
 
-            # hmac-sha1_gen end compare
+            #  pbkdf2_gen
+            pmk = (pbkdf2_hmac(hash_name='sha1', password=line, salt=essid, iterations=4096, dklen=32))
+
+            #  hmac-sha1_gen end compare
             trans_5C = "".join(chr(x ^ 0x5c) for x in xrange(256))
             trans_36 = "".join(chr(x ^ 0x36) for x in xrange(256))
             blocksize = sha1().block_size
             pmk += chr(0) * (blocksize - len(pmk))
             o_key_pad = pmk.translate(trans_5C)
             i_key_pad = pmk.translate(trans_36)
-            #compare hmac-sha1 with PMKID, if match show result end kill child process. 
+            #  compare hmac-sha1 with PMKID, if match show result end kill child process.
             if (sha1(o_key_pad + sha1(i_key_pad + msg).digest()).hexdigest()[:32]) == pmkid:
-            
                 end = datetime.datetime.now()
-                elapsed = end - start                
+                elapsed = end - start
 
-                print("[!] Cracked!!! BSSID...: " + essid + "\n               PSK...: " + line + "\n               Time elapsed...: " + str(elapsed)[:-3] + "\n" )
+                print(
+                    "[!] Cracked!!! BSSID...: " + essid + "\n               PSK...: " + line +
+                        "\n               Time elapsed...: " + str(elapsed)[:-3] + "\n")
+
+                sem.release()
 
                 sys.exit()
+
+        print( "[!] Ap " + essid + " exhausted")
+
+        sem.release()
 
 
 parser = argparse.ArgumentParser()
@@ -45,28 +53,43 @@ results = parser.parse_args()
 hash_file = results.hash_file
 wordlist = results.wordlist_file
 
+if hash_file is None or wordlist is None:
+    parser.print_help()
+    sys.exit()
+
+
 if __name__ == '__main__':
+    print("running...\n")
     try:
         wf = open(wordlist, 'r')
     except IOError:
-        print('[!] I cannot load this file: "'+wordlist+'"!')
+        print("[!] I cannot load this file: \"" + wordlist + "!")
         sys.exit()
     if hash_file is not None:
         try:
             to_crack = [line.strip()
-            for line in open(hash_file)]
+                        for line in open(hash_file)]
+
         except IOError:
-            print('[!] I cannot load this file: "'+hash_file+'"!')
+            print("[!] I cannot load this file: \"" + hash_file + "!")
             sys.exit()
 
-        #Creates processes for each hash
+        to_crack = filter(None, to_crack) 
+
+        #  Creates processes for each hash
+
+        maxconnections = multiprocessing.cpu_count() #  number of spawning to_bute() function, based on your processor cores
+
+        sem = multiprocessing.BoundedSemaphore(value=maxconnections)
+        
         for hashes in to_crack:
+
             hcl = hashes.strip().split('*')
             pmkid = (hcl[0])
-            macAp = (hcl[ 1] )
+            macAp = (hcl[1])
             macCli = (hcl[2])
             essid = (hcl[3]).decode('hex')
-            msg = ("504d4b204e616d65"+macAp+macCli).decode("hex")# Atom's magic numbers :)
+            msg = ("504d4b204e616d65" + macAp + macCli).decode("hex")  #  Atom's magic numbers :)
 
-            mp = Process(target=to_brute, args=(pmkid, essid, msg))
+            mp = multiprocessing.Process(target=to_brute, args=(sem,))
             mp.start()
